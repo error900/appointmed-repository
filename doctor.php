e711cd322923ec1f03f0c75f8900b0eb9a4acfb0
bfmac <abe_macion@yahoo.com>
2015-02-26 07:46:36 +0800

    subscribe
    ung subscribe and unsubscribe module

--- doctor.php
+++ doctor.php
@@ -22,9 +22,9 @@
             //patient
             $username = $_SESSION['username'];
             $result = mysqli_query($con, "SELECT * FROM patient WHERE username LIKE '$username'" );
-            $row =  mysqli_fetch_array($result);
-            $patient = $row['patient_id'];
-            $patient_n = $row['patient_name'];
+            $p_row =  mysqli_fetch_array($result);
+            $patient = $p_row['patient_id'];
+            $patient_n = $p_row['patient_name'];
 
             //clinic
             $c_result = mysqli_query($con, "SELECT * FROM clinic WHERE doctor_id LIKE '$doctor_id'");
@@ -83,6 +83,12 @@
                             <li><?php echo $row['email']; ?></li>
                             <li>Status: <?php echo strtoupper($row['doctor_status']);?></li>
                         </ul>
+                        <form action="subscribe.php" method="post" class="subs">
+                            <input type="hidden" name="doctor" value="<?php echo $row['doctor_id']?>">
+                            <input type="hidden" name="patient" value="<?php echo $p_row['patient_id']?>">
+                            <input type="submit" class="btn btn-default" name="subs" value="Subscribe">
+                            <input type="submit" class="btn btn-default" name="unsubs" value="Unsubscribe">
+                        </form>
                     </div>
                 </div>            
             </div>
---
+++ subscribe.php
@@ -0,0 +1,20 @@
+<?php 
+   include 'connectdatabase.php';
+    $doc = $_POST['doctor'];
+    $pat = $_POST['patient'];
+
+    $sql2 = mysqli_query($con, "SELECT * FROM subscribe WHERE doctor_id LIKE '$doc' AND patient_id LIKE '$pat'" );
+    $count=mysqli_num_rows($sql2);
+
+    if($count >= 1){
+        echo "<script> alert('you are already subscribed to this doctor');</script>";
+    } else {
+        $sql = "INSERT INTO subscribe (doctor_id, patient_id) 
+        VALUES ('$doc', '$pat')";
+        mysqli_query($con, $sql) or die (mysqli_error());
+        header("location: doctor.php");
+        echo "<script> alert('you are subscribed');</script>";
+    }
+    mysqli_close($con);
+    
+?>
