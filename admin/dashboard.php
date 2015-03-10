0d9542c8d443f8f320244a6a93a39761be17b31e
stolenpancakes <judefvargas@gmail.com>
2015-03-10 09:09:46 +0800

    asdads

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,6 +3,7 @@
 	<?php
 		$title = "Admin | Dashboard";
 		include 'include/head.php';
+		include '../connectdatabase.php';
    ?>
 
 	<?php   
@@ -10,11 +11,16 @@
 		$loggedIn = $_SESSION['loggedIn'];
 		$account_type = $_SESSION['account_type'];
 		if($loggedIn == false )
-			header("location: index.php");
+			header("location: admin/index.php");
 		else if($account_type != 'Admin')
-            header("location: index.php");
-	?>
-  <body class="ecf0f1-bg">
+            header("location: admin/index.php");
+
+        $account_sql = mysqli_query($con, "SELECT * FROM account");
+    //    $account_row = mysqli_fetch_array($account_sql);
+
+      ?>
+
+
 	<nav class="navbar navbar-default navbar-fixed-top">
 	  <div class="container-fluid">
 		<div class="navbar-header">
@@ -101,126 +107,28 @@
 			<table class="table table-striped">
 			  <thead>
 				<tr>
-				  <th>#</th>
-				  <th>Header</th>
-				  <th>Header</th>
-				  <th>Header</th>
-				  <th>Header</th>
+			      <th>ID</th>
+				  <th>Username</th>
+				  <th>Name</th>
+				  <th>Email</th>
+				  <th>Account Status</th>
 				</tr>
 			  </thead>
 			  <tbody>
-				<tr>
-				  <td>1,001</td>
-				  <td>Lorem</td>
-				  <td>ipsum</td>
-				  <td>dolor</td>
-				  <td>sit</td>
-				</tr>
-				<tr>
-				  <td>1,002</td>
-				  <td>amet</td>
-				  <td>consectetur</td>
-				  <td>adipiscing</td>
-				  <td>elit</td>
-				</tr>
-				<tr>
-				  <td>1,003</td>
-				  <td>Integer</td>
-				  <td>nec</td>
-				  <td>odio</td>
-				  <td>Praesent</td>
-				</tr>
-				<tr>
-				  <td>1,003</td>
-				  <td>libero</td>
-				  <td>Sed</td>
-				  <td>cursus</td>
-				  <td>ante</td>
-				</tr>
-				<tr>
-				  <td>1,004</td>
-				  <td>dapibus</td>
-				  <td>diam</td>
-				  <td>Sed</td>
-				  <td>nisi</td>
-				</tr>
-				<tr>
-				  <td>1,005</td>
-				  <td>Nulla</td>
-				  <td>quis</td>
-				  <td>sem</td>
-				  <td>at</td>
-				</tr>
-				<tr>
-				  <td>1,006</td>
-				  <td>nibh</td>
-				  <td>elementum</td>
-				  <td>imperdiet</td>
-				  <td>Duis</td>
-				</tr>
-				<tr>
-				  <td>1,007</td>
-				  <td>sagittis</td>
-				  <td>ipsum</td>
-				  <td>Praesent</td>
-				  <td>mauris</td>
-				</tr>
-				<tr>
-				  <td>1,008</td>
-				  <td>Fusce</td>
-				  <td>nec</td>
-				  <td>tellus</td>
-				  <td>sed</td>
-				</tr>
-				<tr>
-				  <td>1,009</td>
-				  <td>augue</td>
-				  <td>semper</td>
-				  <td>porta</td>
-				  <td>Mauris</td>
-				</tr>
-				<tr>
-				  <td>1,010</td>
-				  <td>massa</td>
-				  <td>Vestibulum</td>
-				  <td>lacinia</td>
-				  <td>arcu</td>
-				</tr>
-				<tr>
-				  <td>1,011</td>
-				  <td>eget</td>
-				  <td>nulla</td>
-				  <td>Class</td>
-				  <td>aptent</td>
-				</tr>
-				<tr>
-				  <td>1,012</td>
-				  <td>taciti</td>
-				  <td>sociosqu</td>
-				  <td>ad</td>
-				  <td>litora</td>
-				</tr>
-				<tr>
-				  <td>1,013</td>
-				  <td>torquent</td>
-				  <td>per</td>
-				  <td>conubia</td>
-				  <td>nostra</td>
-				</tr>
-				<tr>
-				  <td>1,014</td>
-				  <td>per</td>
-				  <td>inceptos</td>
-				  <td>himenaeos</td>
-				  <td>Curabitur</td>
-				</tr>
-				<tr>
-				  <td>1,015</td>
-				  <td>sodales</td>
-				  <td>ligula</td>
-				  <td>in</td>
-				  <td>libero</td>
-				</tr>
+				<?php 
+				while($row = mysqli_fetch_array($account_sql)){
+					$username  = $row['username'];
+					$d_result = mysqli_query($con, "SELECT * FROM doctor WHERE username LIKE '$username'" );
+                    $doc =  mysqli_fetch_array($d_result);
+					echo '<tr>';
+					echo '<td>'.$row['username'].'</td>';
+					echo '<td>'.$row['account_type'].'</td>';
+					echo '<td>'.$row['account_type'].'</td>';
+					echo '<td>'.$row['account_type'].'</td>';
+					echo '<td>'.$row['account_status'].'</td>';
+					echo '<tr>';
+					 }
+				?>
 			  </tbody>
 			</table>
 		  </div>
