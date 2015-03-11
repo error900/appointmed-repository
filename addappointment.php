2626e38b97411bab5d2ddd35398b15835c82d581
stolenpancakes <judefvargas@gmail.com>
2015-03-11 19:38:26 +0800

    Changes to appointment creation and editing

--- addappointment.php
+++ addappointment.php
@@ -10,19 +10,25 @@ if(isset($_POST['submit'])){
 	$message="A patient has requested an appointment.";
 	$n_id="n1004";
 	$indicator="patient";
+	$date = date('m/d/Y', strtotime($date));
+	$date_today = date('m/d/Y');
+	
+	if($date >= $date_today){
+		$sql = "INSERT INTO appointment (doctor_id, patient_id, appoint_date, appointment_status, remarks, clinic_id) 
+		VALUES('$doctor_id', '$patient_id', '$date', '$appointment_status', '$remarks','$clinic_id')";
 
+		$notif = "INSERT INTO notification (indicator, doctor_id, patient_id, notif_id, notification) 
+		VALUES('$indicator', '$doctor_id', '$patient_id', '$n_id', '$message')";
 
-	$sql = "INSERT INTO appointment (doctor_id, patient_id, appoint_date, appointment_status, remarks, clinic_id) 
-	VALUES('$doctor_id', '$patient_id', '$date', '$appointment_status', '$remarks','$clinic_id')";
-
-	$notif = "INSERT INTO notification (indicator, doctor_id, patient_id, notif_id, notification) 
-	VALUES('$indicator', '$doctor_id', '$patient_id', '$n_id', '$message')";
-
-	if (!(mysqli_query($con, $sql)) & !(mysqli_query($con, $notif)) ) {
-	  	die('Error: ' . mysqli_error($con));
+		if (!(mysqli_query($con, $sql)) & !(mysqli_query($con, $notif)) ) {
+		  	die('Error: ' . mysqli_error($con));
+		}
+		header("location: appointment.php");
+	}else{
+		echo '<script>alert("Please change the date")</script>';
+		echo "<script> location.replace('doctor.php?id=".$doctor_id."') </script>";
 	}
-	echo '<script>alert("You successfully added an appointment")</script>';
-	header("location: appointment.php");
+
 }else
 	echo "<script>alert('Error')</script>";
 	mysqli_close($con);
--- editappointment.php
+++ editappointment.php
@@ -4,14 +4,19 @@ if(isset($_POST['submit'])){
 	$appdate = $_POST['appdate'];
 	$appid = $_POST['appointment_id'];
 	$appdate = date('m/d/Y', strtotime($appdate));
-	echo $appdate . '<br/>';
-	echo $appid;
-	$sql = "UPDATE appointment SET appoint_date = '$appdate' WHERE appointment_id= $appid";
+	$current_date = date('m/d/Y');
 
-	if (!(mysqli_query($con, $sql))) {
-	  	die('Error: ' . mysqli_error($con));
+	if($appdate >= $current_date){
+		$sql = "UPDATE appointment SET appoint_date = '$appdate' WHERE appointment_id= $appid";
+
+		if (!(mysqli_query($con, $sql))) {
+		  	die('Error: ' . mysqli_error($con));
+		}
+		header("location: appointment.php");
+	}else{
+		echo '<script>alert("Please change the date")</script>';
+		echo "<script> location.replace('appointment.php') </script>";
 	}
-	header("location: appointment.php");
 }else
 	echo "<script>alert('Error')</script>";
 	mysqli_close($con);
