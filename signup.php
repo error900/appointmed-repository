2566f8d988803cc5001db3c699fe5701f2ee89db
tsukuyomii <denmarkmacam@gmail.com>
2015-03-16 04:57:20 +0800

    signup tweaks

--- signup.php
+++ signup.php
@@ -59,20 +59,17 @@
         </nav>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-xs-12 col-md-5 visible-md visible-lg">
-                    <h1 class="text-center">************</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="col-xs-12 col-md-4 col-md-offset-1 hidden-sm hidden-xs">
+                    <h1 class="text-center row-header-lc">Signup and create appointments online!</h1>
+                    <div class="signup-ad">
+                        <p>Look for available clinics and doctors online</p>
+                        <p>Be the first on the list by creating appointments</p>
+                        <p>Get notifications from your doctor via text</p>
+                    </div>
                 </div>
-                <div class="col-xs-12 col-md-5 col-md-offset-1">
-                    <h1 class="text-center">Signup</h1>
+                <div class="col-xs-12 col-md-4 col-md-offset-1">
+                    <h1 class="text-center row-header-lc hidden-lg hidden-md">Signup Now</h1>
                     <div class="signup-form">
-                        
-
                          <form method='post' name='form1' action="register.php">
                             <div class="input-group">
                                 <input type="text" class="form-control" name="firstname" placeholder="First Name" required=""/>
@@ -80,9 +77,10 @@
                                 <input type="email" class="form-control" name="email" placeholder="Email Address" required=""/>
                                 <input type="text" class="form-control" name="contact" placeholder="Contact Number" required=""/>
                                 <input type="text" class="form-control" name="occupation" placeholder="Occupation" required=""/>
-                          <div>Birthdate</div>
-                                Month: <select name=birth_month>
-                                        <option selected value=1>January
+                          <label><i class="fa fa-birthday-cake"></i>Birthday</label>
+                                <select name="birth_month" class="form-control">
+                                        <option selected>Month
+                                        <option value=1>January
                                         <option value=2> February
                                         <option value=3>March
                                         <option value=4>April
@@ -95,14 +93,16 @@
                                         <option value=11>November
                                         <option value=12>December 
                                     </select>
-                               Day: <select name=birth_day>
+                               <select name="birth_day" class="form-control">
+                                        <option selected>Day
                                         <?php for($i=1; $i<=31; $i++){ ?>
                                         <option value="<?php echo $i; ?>">
                                             <?php echo $i; ?>
                                         </option> 
                                             <?php } ?>
                                     </select>
-                               Year: <select name=birth_year>
+                               <select name="birth_year" class="form-control">
+                                        <option selected>Year
                                         <?php for($i=1900; $i<=2015; $i++){ ?>
                                         <option value="<?php echo $i; ?>">
                                             <?php echo $i; ?>
@@ -116,7 +116,7 @@
                                 required pattern="(?=.*[a-z])(?=.*[A-Z]).{6,}" 
                                 onchange="this.setCustomValidity(this.validity.patternMismatch ? this.title : '');
                                 if(this.checkValidity()) form1.password2.pattern = this.value;" required=""/>  
-                                <p class="passwordReq">*Your password must contain uppercase and lowercase letters, and it should not be lower than 6 characters. </p>
+                                <p class="passwordReq">Your password must contain uppercase and lowercase letters, and it should not be lower than 6 characters. </p>
                                 
                                 <input type="password" title="Passwords do not matcch" class="form-control" name="password2" placeholder="Confirm Password" 
                                 onchange=" this.setCustomValidity(this.validity.patternMismatch ? this.title : '');"
@@ -125,8 +125,6 @@
                                 <input class="btn btn-default login-btn" type="submit" value="Submit" name="submit"/>
                             </div>
                         </form>
-
-
                     </div>
                 </div>
             </div>
