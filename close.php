bc7d33aafc7930e3731437094b6e325786826c55
bfmac <abe_macion@yahoo.com>
2015-03-16 08:28:54 +0800

    close
    edited

--- close.php
+++ close.php
@@ -2,14 +2,15 @@
 	include 'connectdatabase.php';
 	$app_id= mysqli_real_escape_string($con, $_GET['id']);
 	$appointment_status = 'Cancelled';
+	$date = date('Y-m-d');
 	$doc = mysqli_real_escape_string($con, $_GET['doc']);
 	$pat = mysqli_real_escape_string($con, $_GET['pat']);
 	$message="A patient has cancelled his appointment.";
 	$n_id="n1004";
 	$indicator="patient";
 
- 	$notif = "INSERT INTO notification (indicator, doctor_id, patient_id, notif_id, notification) 
-	VALUES('$indicator','$doc', '$pat', '$n_id', '$message')";
+ 	$notif = "INSERT INTO notification (indicator, doctor_id, patient_id, legend_id, notification_date, notification) 
+	VALUES('$indicator','$doc', '$pat', '$n_id', '$date', '$message')";
 	mysqli_query($con, $notif) or die (mysqli_error($con));
 
 	$sql = "UPDATE appointment SET appointment_status ='Cancelled' WHERE appointment_id = '$app_id' ";
