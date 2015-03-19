28d9e08934bf9020a70d22196e2a6f200b617cba
tsukuyomii <denmarkmacam@gmail.com>
2015-03-19 08:03:18 +0800

    Revert "editappointment"
    This reverts commit 4f1bf59bb7d9ffc5dcf54cb3c1dc576fc884854b.

--- editappointment.php
+++ editappointment.php
@@ -2,25 +2,14 @@
 include 'connectdatabase.php';
 if(isset($_POST['submit'])){
 	$appdate = $_POST['appdate'];
-	$pat = $_POST['patient_id'];
-	$doc = $_POST['doctor_id'];
 	$appid = $_POST['appointment_id'];
-	$appdate = date('Y-m-d', strtotime($appdate));
-	$current_date = date('Y-m-d');
-	$message="A patient has rescheduled his appointment to you.";
-	$n_id="n1002";
-	$indicator="patient";
+	$appdate = date('m/d/Y', strtotime($appdate));
+	$current_date = date('m/d/Y');
 
 	if($appdate >= $current_date){
 		$sql = "UPDATE appointment SET appoint_date = '$appdate' WHERE appointment_id= $appid";
 
-
-		$notif = "INSERT INTO notification (indicator, doctor_id, patient_id, legend_id, notification_date, notification) 
-		VALUES('$indicator','$doc', '$pat', '$n_id', '$current_date', '$message')";
-		//mysqli_query($con, $notif) or die (mysqli_error($con));
-
-
-		if (!(mysqli_query($con, $sql)) & !(mysqli_query($con, $notif))) {
+		if (!(mysqli_query($con, $sql))) {
 		  	die('Error: ' . mysqli_error($con));
 		}
 		header("location: appointment.php");
