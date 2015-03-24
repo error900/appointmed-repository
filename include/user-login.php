ab3c61021d6a059becad4e6f0b98f9f3ffccf06f
tsukuyomii <denmarkmacam@gmail.com>
2015-03-24 23:46:57 +0800

    asdasdasd

--- include/user-login.php
+++ include/user-login.php
@@ -7,7 +7,7 @@
             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
             <h4 class="modal-title" id="myModalLabel">Login</h4>
         </div>
-        <div class="modal-body">
+        <div class="modal-body e4e8e9-bg">
             <div class="usr-login">
                 <form method="post" action="login.php">
                     <div class="input-group">
