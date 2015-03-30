bbe7cd1071e76fb61c87aa5f7003123e688b2174
tsukuyomii <denmarkmacam@gmail.com>
2015-03-30 23:03:49 +0800

    random changes 3/30/2015

--- admin/exportall.php
+++ admin/exportall.php
@@ -20,51 +20,37 @@
     //    $account_row = mysqli_fetch_array($account_sql);
     ?>
 
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">BenguetLabs</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Dashboard</a></li>
-                    <div class="btn-group navbar-right signedin">
-                        <button type="button" class="btn btn-default btn-lg btn-noborder dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                            admin
-                            <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Profile</a></li>
-                            <li><a href="#">Settings</a></li>
-                            <li><a href="#">Help</a></li>
-                            <li class="divider"></li>
-                            <li><a href="logout.php"><i class="fa fa-power-off"></i>    logout</a></li>
-                        </ul>
-                    </div>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container-fluid">
-        <div class="row">
+    <body class="e4e8e9-bg">
             <?php
-            include 'include/sidebar-navigation.php';
+            include 'include/admin-nav-start.php';
             ?>
+            <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">Help</a></li>
+                <li class="divider"></li>
+                <li><a href="logout.php"><i class="fa fa-power-off"></i>    logout</a></li>
+            </ul>
+            <?php
+            include 'include/admin-nav-end.php';
+            ?>s
+
+        <div class="container-fluid">
+            <div class="row">
+                <?php
+                include 'include/sidebar-navigation.php';
+                ?>
+            </div>
         </div>
-    </div>
-    <div class="col-sm-9 col-sm-offset-3 col-md-4 col-md-offset-2 main">
-        <h1 class="page-header">Export</h1>
-        <form method="post" action="export_to_file.php">
-            <input type="submit" class="btn btn-default export-btn btn-noborder" name="export" value="Export Users List">
-        </form>
-        <script type="text/javascript" src="js/listslide.js"></script>
+        <div class="col-sm-9 col-sm-offset-3 col-md-4 col-md-offset-2 main">
+            <h1 class="page-header">Export</h1>
+            <form method="post" action="export_to_file.php">
+                <input type="submit" class="btn btn-default export-btn btn-noborder" name="export" value="Export Users List">
+            </form>
+        </div>
+            <?php
+            include 'include/scripts.php';
+            ?>
+            <script type="text/javascript" src="js/listslide.js"></script>
     </body>
 </html>
--- admin/include/sidebar-navigation.php
+++ admin/include/sidebar-navigation.php
@@ -1,7 +1,7 @@
 <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar main-nav-label">
         <li class="sidebar-list">
-            <label>Navigation item one</label>
+            <label><i class="fa fa-navicon"></i>Navigation item one</label>
             <ul class="nav nav-sidebar sidebar-items-list">
                 <li><a href="dashboard.php">Dashboard</a></li>
                 <li><a href="#">Analytics</a></li>
@@ -12,7 +12,7 @@
     </ul>
     <ul class="nav nav-sidebar main-nav-label">
         <li class="sidebar-list">
-            <label>Navigation item two</label>
+            <label><i class="fa fa-navicon"></i>Navigation item two</label>
             <ul class="nav nav-sidebar sidebar-items-list">
                 <li><a href="popdoc.php">Add Doctor</a></li>
                 <li><a href="remove_user.php">Remove user</a></li>
@@ -22,7 +22,7 @@
     </ul>
     <ul class="nav nav-sidebar main-nav-label">
         <li class="sidebar-list">
-            <label>Navigation item three</label>
+            <label><i class="fa fa-navicon"></i>Navigation item three</label>
             <ul class="nav nav-sidebar sidebar-items-list">
                 <li><a href="approve.php">Approve Users</a></li>
             </ul>
