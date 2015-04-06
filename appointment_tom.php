3b3a03555e3e431d4a806f257d65c6efc7b52626
tsukuyomii <denmarkmacam@gmail.com>
2015-04-06 19:17:22 +0800

    patient module fix part 2

--- appointment_tom.php
+++ appointment_tom.php
@@ -70,47 +70,12 @@
         <!-- /navigation -->           
         <div class="container-fluid" id="appointments-user">
             <div class="row">
-                <?php if (mysqli_num_rows($p_result) >= 1) { 
-                echo '<div class="col-md-12">
+                <div class="col-md-12">
                     <h1 class="text-center row-header-fff">&mdash; Tomorrow &mdash;</h1>
-                </div>';
-
-                    while ($d_row = mysqli_fetch_array($p_result)){
-                    $app_id = $d_row['appointment_id'];
-                    $doctor = $d_row['doctor_id'];
-                    $date = $d_row['appoint_date'];
-
-                    $d_result = mysqli_query($con, "SELECT * FROM doctor WHERE doctor_id LIKE '$doctor'" );
-                    $doc =  mysqli_fetch_array($d_result);
-                    echo '<div class="col-xs-12 col-md-6 col-lg-3" id="'.$d_row['appointment_id'].'">';
-                    echo "<div class='panel panel-default' id='asd'><div class='panel-heading appointment-date' >";
-                            echo $date;
-                            echo "<a href=\"close.php?id=$d_row[appointment_id]&doc=$doctor&pat=$patient_id\" onclick='return confirm(\"Do you want to cancel this appointment?\")' title=\"Cancel\"><i class=\"fa fa-remove fa-lg delete-btn\"></i></a></div>
-                            <div class=\"panel-body\">";
-                            echo '<p class="appointment-dr-name">Dr. ' . $doc['doctor_name'] . '</p>';
-                            echo "</div><div class='appmnt-pnl-btn'>
-                            <a class='btn btn-block btn-inverse appo tooltip' data-toggle='modal' data-target='.bs-example-modal-sm' data-id='".$app_id."' data-doctor-id='".$doctor."' title='edit this appointment'><span title='asdsad'><i class='fa fa-pencil'></i></span> Edit</a>";
-                            echo '<p class="appointment-specs">' . $doc['specialization'] . '</p></div></div>';
-                    echo '</div>';
-                    }
-                }else{
-                            echo '<div class="col-md-12">
-                            <h1 class="text-center row-header-fff">&mdash; Tomorrow &mdash;</h1>
-                        </div>';
-                            echo '<div class="col-xs-12 col-md-10 col-md-offset-1">
-                                <div class="alert alert-warning" role="alert">
-                                <strong>There are no appointments!</strong> Better check yourself, youre not looking too good.</div>
-                                </div>';
-                            echo '<div class="col-xs-12 col-md-10 col-md-offset-1">
-                                <div class="alert alert-info alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span></button>
-                            To know more about creating appointments please read the following
-                            <strong> <a href="help.php" class="alert-link">instructions</a></strong>.
-                            </div>
-                            </div>';
-                        }
-                ?>
+                </div>
+                <?php 
+                include  'include/appointment-panel.php';
+                 ?>
             </div>
         </div>
 
