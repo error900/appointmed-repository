fae89d127cf4586b1fdecef2b69ada223e240feb
tsukuyomii <denmarkmacam@gmail.com>
2015-04-09 13:44:15 +0800

    layout changes part 1 (patient)

--- appointment.php
+++ appointment.php
@@ -47,28 +47,24 @@
             <?php
             include 'include/pt-nav-start.php';
             ?>
-            <ul class="nav navbar-nav">
-                <li class="dropdown active">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Appointments <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Today</a></li>
-                        <li><a href="appointment_tom.php">Tomorrow</a></li>
-                        <li><a href="appointment_week.php">This Week</a></li>
-                        <li><a href="appointment_month.php">This Month</a></li>
-                    </ul>
-                </li>
-                <li><a href="notifications.php">Notifications <span class="badge"><?php echo $notif_count ?></span></a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">History <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="appointments_done.php">Appointments Done</a></li>
-                        <li><a href="cancelled_appointments.php">Cancelled Appointments</a></li>
-                    </ul>
-                </li>
-                <li class="nav-button navbar-right">
-                    <button type="button" class="btn btn-default btn-noborder edit-profile-btn" data-toggle="modal" data-target=".bs-pt-edit-profile-modal-lg" data-id="'.$appointment_id.'" data-patient-id="'.$patient_id.'">
-                    <i class="fa fa-pencil"></i>Edit Profile</button>
-                </li>
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown active">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-calendar fa-lg"><span class="caret"></span></i>Appointments</a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="appointment.php">Today</a></li>
+                                <li><a href="appointment_tom.php">Tomorrow</a></li>
+                                <li><a href="appointment_week.php">This Week</a></li>
+                                <li><a href="appointment_month.php">This Month</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="notifications.php"><i class="fa fa-bell fa-lg"><span class="badge"><?php echo $notif_count?></span></i>Notifications </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-history fa-lg"><span class="caret"></span></i>History</a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="appointments_done.php">Appointments Done</a></li>
+                                <li><a href="cancelled_appointments.php">Cancelled Appointments</a></li>
+                            </ul>
+                        </li>
                 <?php
                 include 'include/pt-nav-end.php';
                 ?>     
--- include/pt-nav-end.php
+++ include/pt-nav-end.php
@@ -1,39 +1,43 @@
-<form class="navbar-form navbar-right" method="post" role="search">
-    <div class="input-group search-bar">
-        <ul class="dropdown-menu divResult" role="menu">
-        </ul>
-        <input type="text" class="form-control" name="search" placeholder="search doctor" id="inputSearch">
+            <li class="nav-button navbar-right">
+                <button type="button" class="btn btn-default btn-noborder green-btn" data-toggle="modal" data-target=".bs-pt-edit-profile-modal-lg" data-id="'.$appointment_id.'" data-patient-id="'.$patient_id.'">
+                <i class="fa fa-pencil"></i>Update Info</button>
+            </li>
+            <form class="navbar-form navbar-right" method="post" role="search">
+                <div class="input-group search-bar">
+                    <ul class="dropdown-menu divResult" role="menu">
+                    </ul>
+                    <input type="text" class="form-control" name="search" placeholder="search doctor" id="inputSearch">
 
-        <span class="input-group-btn">
-            <button type="submit" class="btn"><i class="fa fa-search"></i></button>
-        </span>
-    </div>
-</form>
-</ul>
-<ul class="nav navbar-nav navbar-right signedin">
-    <div class="btn-group navbar-right">
-        <button type="button" class="btn btn-default btn-lg btn-noborder dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-            <div class="signedin-photo">
-                <img src="img/profile/<?php
-                $file = "img/profile/" . $patient_id . ".jpg";
-                if (file_exists($file)) {
-                    echo $patient_id;
-                } else {
-                    echo 'profile_patient';
-                }
-                ?>.jpg" class="img-responsive">
-            </div>
-<?php echo $patient_n ?>
-            <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu" role="menu">
-            <li><a href="patient.php">Profile</a></li>
-            <li><a href="help.php">Help</a></li>
-            <li class="divider"></li>
-            <li><a href="logout.php"><i class="fa fa-power-off"></i>logout</a></li>
-        </ul>
-    </div>
-</ul>
-</div><!-- /.navbar-collapse -->
-</div><!-- /.container-fluid -->
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn"><i class="fa fa-search"></i></button>
+                    </span>
+                </div>
+            </form>
+            </ul>
+            <ul class="nav navbar-nav navbar-right signedin">
+                <div class="btn-group navbar-right">
+                    <button type="button" class="btn btn-default btn-lg btn-noborder dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        <div class="signedin-photo">
+                            <img src="img/profile/<?php
+                            $file = "img/profile/" . $patient_id . ".jpg";
+                            if (file_exists($file)) {
+                                echo $patient_id;
+                            } else {
+                                echo 'profile_patient';
+                            }
+                            ?>.jpg" class="img-responsive">
+                        </div>
+            <?php echo $patient_n ?>
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="patient.php">Profile</a></li>
+                        <li><a href="help.php">Help</a></li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-power-off"></i>logout</a></li>
+                    </ul>
+                </div>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
