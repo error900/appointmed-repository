aee3248e2c3c390f275fcefc7bfa0d649d14dd73
bfmac <abe_macion@yahoo.com>
2015-04-21 22:23:44 +0800

    referral
    yan na ung referral.. check nyo na lng ang db..

--- referral.php
+++ referral.php
@@ -7,22 +7,36 @@ if (isset($_POST['submit'])) {
     $referred_id = $_POST['referred_id'];
     $appointment_id = $_POST['appointment_id'];
 
+    $p_result = mysqli_query($con, "SELECT * FROM patient WHERE patient_id LIKE '$patient_id'" );
+    $p_row =  mysqli_fetch_array($p_result);
     $d_result = mysqli_query($con, "SELECT * FROM doctor WHERE doctor_id LIKE '$doctor_id'");
     $d_row = mysqli_fetch_array($d_result);
     $rd_result = mysqli_query($con, "SELECT * FROM doctor WHERE doctor_id LIKE '$referred_id'");
     $rd_row = mysqli_fetch_array($rd_result);
 
-    $message = "You have been referred by Dr. " . $d_row['doctor_name'] . " to <strong>Dr. " . $rd_row['doctor_name'] . '</strong>';
-    $n_id = "n1002";
-    $indicator = "doctor";
-    $notif = "INSERT INTO notification (indicator, doctor_id, patient_id, legend_id, notification_date, notification) 
-	VALUES('$indicator','$doctor_id', '$patient_id', '$n_id', '$date' , '$message')";
-    mysqli_query($con, $notif) or die(mysqli_error($con));
+    //patient notification
+    $message="You have been referred by Doctor ".$d_row['doctor_name']." to <a href=\"doctor.php?id=".$referred_id."\">Doctor ".$rd_row['doctor_name']."</a>";
+    $n_id="n1002";
+    $indicator="doctor";
+    $notif_patient = "INSERT INTO notification (indicator, doctor_id, patient_id, legend_id, notification_date, notification) 
+    VALUES('$indicator','$doctor_id', '$patient_id', '$n_id', '$date' , '$message')";
+    mysqli_query($con, $notif_patient) or die (mysqli_error($con));
+    //patient notification end
 
-    $update_appointment = "UPDATE appointment SET doctor_id = '$referred_id', appointment_status = 'Referred' WHERE appointment_id = '$appointment_id'";
-    $sql = "INSERT INTO referred (doctor_id, patient_id, referred_id) VALUES('$doctor_id', '$patient_id', '$referred_id')";
 
-    if (!(mysqli_query($con, $update_appointment)) || !mysqli_query($con, $sql)) {
+    //doctor notification
+    $message= $p_row['patient_name']." has been referred to you by Doctor ".$d_row['doctor_name'].".";
+    $n_id="n1002";
+    $indicator="patient";
+    $notif_doctor = "INSERT INTO notification (indicator, doctor_id, patient_id, legend_id, notification_date, notification) 
+    VALUES('$indicator','$referred_id', '$patient_id', '$n_id', '$date' , '$message')";
+    mysqli_query($con, $notif_doctor) or die (mysqli_error($con));
+    //doctor notification end
+
+   $delete_appointment = "DELETE FROM appointment WHERE appointment_id = '$appointment_id'";
+    $sql = "INSERT INTO referpatient (patient_id, original_doctor, substitute_doctor) VALUES('$patient_id','$doctor_id', '$referred_id')";
+
+    if (!(mysqli_query($con, $delete_appointment)) || !mysqli_query($con, $sql)) {
         die('Error: ' . mysqli_error($con));
     }
     header("location: schedules.php");
