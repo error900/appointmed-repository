97d23d0e903212113781d92267a962d6bb0e3773
bfmac <abe_macion@yahoo.com>
2015-04-25 00:42:28 +0800

    email remarks
    email remarks to patient if the appointment is completed

--- remarks.php
+++ remarks.php
@@ -6,9 +6,17 @@ if (isset($_POST['submit'])) {
     $doc = $_POST['doctor_id'];
     $status = $_POST['isComplete'];
     $appointment_id = $_POST['appointment_id'];
-    echo $appointment_id;
-    echo $remarks;
     $sql = "UPDATE appointment SET remarks = '$remarks', appointment_status = '$status' WHERE appointment_id = '$appointment_id' ";
+
+    //patient
+    $p_result = mysqli_query($con, "SELECT * FROM patient WHERE patient_id LIKE '$patient_id'" );
+    $p_row =  mysqli_fetch_array($p_result);
+
+    // email remarks to patients email
+    $email = $p_row['email'];
+    $subject = 'Remarks for your appointment';
+    $headers = 'From: Benguet Labs';
+
 
     if ($status == 'Completed') {
         $date = date("Y-m-d");
@@ -21,6 +29,8 @@ if (isset($_POST['submit'])) {
         $notif = "INSERT INTO notification (indicator, doctor_id, patient_id, legend_id, notification_date, notification) 
 			VALUES('$indicator','$doc', '$pat', '$n_id', '$date', '$message')";
         mysqli_query($con, $notif) or die(mysqli_error($con));
+
+        $sendmail = mail($email, $subject, $remarks, $headers);
     }
 
     if (!(mysqli_query($con, $sql))) {
