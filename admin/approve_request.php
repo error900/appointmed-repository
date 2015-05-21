eed3bf5eee0085a99c8c17d9ffaee4d4ee2d1795
stolenpancakes <judefvargas@gmail.com>
2015-05-21 09:05:23 +0800

    safgdbfdg

--- admin/approve_request.php
+++ admin/approve_request.php
@@ -4,35 +4,13 @@ include '../connectdatabase.php';
 if (isset($_POST['submit'])) {
     if (isset($_POST['select'])) {
         $select = array();
-        $password = substr(md5(uniqid(rand(), true)), 0, 7);
-        $password = hash("sha256", $password);
-        
+
         foreach ($_POST['select'] as $values) {
             $sql = "UPDATE account SET account_status='active', password='$password' WHERE username = '$values'";
-
-
-            $patient = mysqli_query($con, "SELECT * FROM patient WHERE username ='$values'");
-            $p_row =  mysqli_fetch_array($patient);
-
-            //email password
-            $email =  $p_row['email'];
-            $message = '
-            '.$values.',
-
-            Thank You for Registering to Appointmed.
-
-            username: '.$values.'
-            password: '.$password.' 
-
-            You may now login to Appointmed using your username and password.
-            ';
-            $subject = 'Password of you account ';
-            $headers = 'From: Benguet Labs';
 
             if (!(mysqli_query($con, $sql))) {
                 die('Error: ' . mysqli_error($con));
             }
-            $sendmail = mail($email, $subject, $message, $headers);
             
         } header("location: approve.php");
     } else {
--- admin/include/sidebar-navigation.php
+++ admin/include/sidebar-navigation.php
@@ -1,48 +1,14 @@
 <div class="col-sm-3 col-md-2 sidebar">
     <h1 class="page-header nav-menu">Menu</h1>
     <ul class="nav nav-sidebar main-nav-label first">
-        <li class="sidebar-list">
-            <label><i class="fa fa-navicon"></i>General</label>
-            <ul class="nav nav-sidebar sidebar-items-list">
                 <li><a href="dashboard.php">Dashboard</a></li>
                 <li><a href="notifications.php">Notifications</a></li>
-            </ul>
-        </li>
-    </ul>
-    <ul class="nav nav-sidebar main-nav-label">
-        <li class="sidebar-list">
-            <label><i class="fa fa-navicon"></i>Announcements</label>
-            <ul class="nav nav-sidebar sidebar-items-list">
-                <li><a href="announcements.php">Post</a></li>
-                <li><a href="announcement_list.php">History</a></li>
-            </ul>
-        </li>
-    </ul>
-    <ul class="nav nav-sidebar main-nav-label">
-        <li class="sidebar-list">
-            <label><i class="fa fa-navicon"></i>User Management</label>
-            <ul class="nav nav-sidebar sidebar-items-list">
+                <li><a href="announcements.php">Post Announcements</a></li>
+                <li><a href="announcement_list.php">Announcement History</a></li>
                 <li><a href="popdoc.php">Add Doctor</a></li> 
                 <li><a href="remove_user.php">Deactivate Users</a></li>
                 <li><a href="inactive.php">Idle Users</a></li>
-            </ul>
-        </li>
-    </ul>
-    <ul class="nav nav-sidebar main-nav-label">
-        <li class="sidebar-list">
-            <label><i class="fa fa-navicon"></i>Registrations</label>
-            <ul class="nav nav-sidebar sidebar-items-list">
-                <li><a href="approve.php">Pending</a></li>
-            </ul>
-        </li>
-    </ul>
-    <ul class="nav nav-sidebar main-nav-label">
-        <li class="sidebar-list">
-            <label><i class="fa fa-navicon"></i>Current Data</label>
-            <ul class="nav nav-sidebar sidebar-items-list">
-                <li><a href="#">Import</a></li>
-                <li><a href="exportall.php">Export</a></li>
-            </ul>
-        </li>
+                <li><a href="approve.php">Pending Registrations</a></li>
+                <li><a href="exportall.php">Export Data</a></li>
     </ul>
 </div>
