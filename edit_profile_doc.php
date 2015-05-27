69e2a6db3d138f92936c03fd5690bdcbee4bca7a
bfmac <abe_macion@yahoo.com>
2015-05-27 21:23:20 +0800

    rescheduling
    ung queueing nlng problema

--- edit_profile_doc.php
+++ edit_profile_doc.php
@@ -13,8 +13,6 @@ if (isset($_POST['submit'])) {
     $subscribed = mysqli_query($con, "SELECT patient_id FROM subscribe WHERE doctor_id LIKE '$doctor_id'");
     $status_row = mysqli_fetch_array($status);
     if ($doctor_status != $status_row['doctor_status']) {
-        echo $doctor_status;
-        echo $status_row['doctor_status'];
         $message = "Doctor " . $status_row['doctor_name'] . " has changed his/her status to " . $doctor_status;
         $n_id = "n1004";
         $indicator = "doctor";
@@ -25,6 +23,45 @@ if (isset($_POST['submit'])) {
             mysqli_query($con, $notif) or die(mysqli_error($con));
         }
     }
+
+    // Put to next day      start
+    if (($doctor_status != $status_row['doctor_status']) && ($doctor_status == 'Out')){
+        $app = mysqli_query($con, "SELECT * FROM appointment WHERE doctor_id LIKE '$doctor_id' AND appoint_date LIKE '$date' AND appointment_status = 'Inqueue'");
+
+        $clinic = mysqli_query($con, "SELECT * FROM clinic WHERE doctor_id LIKE '$doctor_id'");
+        $clinic_rows = mysqli_fetch_array($clinic);
+        $clinic_id = $clinic_rows['clinic_id'];
+
+        $select_clinic = mysqli_query($con, "SELECT * FROM clinic_schedule NATURAL JOIN clinic WHERE clinic_id LIKE '$clinic_id'");
+        $select_clinic_rows = mysqli_fetch_array($select_clinic);
+        $days = $select_clinic_rows['days'];
+
+        $days = explode('/', $days);
+        foreach($days as $value){
+            $val = (ucfirst(strtolower($value)));
+            array_push($days, $val);
+        }
+
+        $check_day = date('D', strtotime($date));
+
+        if(in_array($check_day, $days)){
+            $key = array_search($check_day, $days);
+            $next = $key+1;
+            $newdate = strtotime ('next '.$days[$next],strtotime($date));
+            $newdate = date ('Y-m-d' , $newdate);
+
+            while($app_result = mysqli_fetch_array($app)){
+                $app_id = $app_result['appointment_id'];
+                $update_appointment = "UPDATE appointment SET appoint_date = '$newdate' WHERE appointment_id LIKE '$app_id'";
+                mysqli_query($con, $update_appointment) or die (mysqli_error($con));
+
+                $update_queue= "UPDATE queue_notif SET appoint_date = '$newdate' WHERE appointment_id LIKE '$app_id'";
+                mysqli_query($con, $update_queue) or die (mysqli_error($con));
+            }
+        }
+    }
+    // end
+
 
     if(!($specialization == '')){
         $specialization = ucfirst(strtolower($specialization));
