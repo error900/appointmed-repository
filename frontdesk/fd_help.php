bd7acc90b6b36ec8ca32e7efd925aa0793efcdcb
epong95 <epong95@gmail.com>
2015-05-30 10:38:49 +0800

    FD HELP <3

--- frontdesk/fd_help.php
+++ frontdesk/fd_help.php
@@ -35,6 +35,19 @@
                 ?>
                 <div class="row">
                     <h2 class="row-header text-center">Help</h2>
+                   </br>
+                    <h4>Doctors Available</h4>
+                    <p>This account is for the fontdesk. The frontdesk personnel can view the available doctors for the day. This will be the guide 
+                    of the personnel if the there is a doctor if a  patient is looking for a specific doctor or specific specialization of a doctor. 
+                    This is also used for walk in patients who are not yet informed about the sytem and has no account yet.</p>
+                   </br>
+                    <h4>Find your Doctor</h4>
+                    <p>Type name of doctor or specialization in search bar above.</p>
+                    <p>Choose and click on the name of your doctor in the result/s shown below the search bar and it will redirect you to the list of the search result. 
+                        there is a + icon where when clicked, you can add a walk in patient to the queue if there is still an available slot.</p>
+                    </br>
+                    <h4>Change Password</h4>
+                    <p>The front desk personnel may change this account's password. The old password is required for verification and also new pasword matching is required</p>
                 </div>
                 <script type="text/javascript" src="js/scrolltop.js"></script>
                 <script type="text/javascript" src="js/search.js"></script>
