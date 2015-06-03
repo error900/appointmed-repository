aaa18f247727222543100e23973b3f2e46452728
bfmac <abe_macion@yahoo.com>
2015-06-03 13:11:15 +0800

    jude paayos na lng ng paging na doctor.. sa isang page lng kasi lahat..
    doctor for the specific day..

--- frontdesk/index.php
+++ frontdesk/index.php
@@ -37,7 +37,7 @@
     $date = date('Y-m-d');
     $username = $_SESSION['username'];
     if(is_int($page1)){
-        $sql = mysqli_query($con, "SELECT * FROM doctor NATURAL JOIN clinic_schedule natural join clinic WHERE doctor_status = 'In' AND clinic_name='Benguet Laboratories' ORDER BY 3 LIMIT $page1,8");
+        $sql = mysqli_query($con, "SELECT * FROM clinic_schedule natural join clinic WHERE clinic_name='Benguet Laboratories'");
     }else{
         echo "<script> alert('Woops you seem lost, let me help you'); </script>";
         echo "<script> location.replace('index.php') </script>";
@@ -65,55 +65,71 @@
                             <h1 class="text-center row-header-black">&mdash; Available Doctors &mdash;</h1>
                         </div>
                         <?php
-                            while($row = mysqli_fetch_array($sql)){
+                        while($row = mysqli_fetch_array($sql)){
                                 $doctor_id = $row['doctor_id'];
                                 $clinic_id = $row['clinic_id'];
-                                $date = date('Y-m-d', strtotime($date));
-                                $available_days = explode("/", $row['days']);
                                 $count_app = mysqli_query($con, "SELECT * FROM appointment where clinic_id like '$clinic_id' AND appoint_date LIKE CURDATE() AND appointment_status = 'Inqueue'");
                                 $count_a= mysqli_num_rows($count_app);
                                 $count_walk_in = mysqli_query($con, "SELECT * FROM walk_in where clinic_id like '$clinic_id' AND appointW_date LIKE CURDATE() AND appointmentW_status = 'Inqueue'");
                                 $count_w = mysqli_num_rows($count_walk_in);
-
                                 $total_count = $count_a + $count_w;
-                        echo "<a href=\"inqueue-details.php?did=$doctor_id&cid=$clinic_id\">";
-                            echo '<div class="col-xs-12 col-md-3">';
-                                echo '<div class="panel panel-default doctor-panel">';
-                                    echo '<div class="panel-heading">';
-                                        echo 'Dr. ' . $row['doctor_name'];
-                                               
-                                        ?>
-                                        <div class="doctor-panel-photo">
-                                            <img class="img-responsive" src="img/profile/<?php
-                                            $file = "img/profile/" . $doctor_id . ".jpg";
-                                            if (file_exists($file)) {
-                                                echo $doctor_id;
-                                            } else {
-                                                echo 'profile';
-                                            }
-                                            ?>.jpg">
-                                        </div>
-                            <?php
 
-                            echo '</div>';
-                                    echo '<div class="panel-body">';
-                                        echo '<p class="clinic-days">'.$row['days'].'</p>';
-                                        echo '<p class="clinic-times">'.$row['time'].'</p>';
-                                        echo '<p class="clinic-inqueue">Total patients in queue: <span>' . $total_count . '</span></p>';
-                                    echo '</div>';
-                                    echo '<div>';
-                                    echo '</div>';
-                       echo '</a>';
-                                    echo '<div class="doctor-panel-btns">';
-                                            // echo "<a href=\"walk_in.php?did=$doctor_id&cid=$clinic_id\" class='tooltip-bottom' data-tooltip='Add to queue' onclick='\return confirm(\"Add patient to queue?\")\'><i class=\"fa fa-plus\"></i></a>";
-                                            echo '<button type="button" class="btn btn-default btn-inverse walk btn-noborder tooltip-bottom" data-tooltip="Add to queue" data-toggle="modal" data-target=".bs-add-modal-sm" data-doc-id="'.$doctor_id.'" data-cli-id="'.$clinic_id.'">add patient</button>';
-                                            echo '<p class="doctor-panel-specs">'.$row['specialization'].'</p>';
-                                    echo '</div>';
-                                echo '</div>';
-                            echo '</div>';
+
+                                $check_day = date('D', strtotime($date));
+                                $days = $row['days'];
+                                $days = explode('/', $days);
+                                foreach($days as $value){
+                                    $val = (ucfirst(strtolower($value)));
+                                    array_push($days, $val);
+                                }
+
+                                if(in_array($check_day, $days)){
+                                    $doctor_id = $row['doctor_id'];
+                                    $doctor = mysqli_query($con, "SELECT * FROM doctor WHERE doctor_id LIKE '$doctor_id'");
+                                    $doctor_rows = mysqli_fetch_array($doctor);
+
+                                    //echo $doctor_rows['doctor_name'].'<br>';
+
+                                echo '<a href="inqueue-details.php?did=$doctor_id&cid=$clinic_id">
+                                        <div class="col-xs-12 col-md-3">
+                                        <div class="panel panel-default doctor-panel">
+                                        <div class="panel-heading">
+                                        Dr. '. $doctor_rows["doctor_name"];                
+                                    ?>
+
+                                    <div class="doctor-panel-photo">
+                                        <img class="img-responsive" src="img/profile/<?php
+                                        $file = "img/profile/" . $doctor_id . ".jpg";
+                                        if (file_exists($file)) {
+                                            echo $doctor_id;
+                                        } else {
+                                            echo 'profile';
+                                        }
+                                        ?>.jpg">
+                                    </div>
+
+                                <?php
+                                echo '</div>
+                                        <div class="panel-body">
+                                        <p class="clinic-days">'.$row['days'].'</p>
+                                        <p class="clinic-times">'.$row['time'].'</p>
+                                        <p class="clinic-inqueue">Total patients in queue: <span>' . $total_count . '</span></p>
+                                        </div>
+                                        <div>
+                                        </div>
+                                        </a>
+                                        <div class="doctor-panel-btns">
+                                                
+                                        <button type="button" class="btn btn-default btn-inverse walk btn-noborder tooltip-bottom" data-tooltip="Add to queue" data-toggle="modal" data-target=".bs-add-modal-sm" data-doc-id="'.$doctor_id.'" data-cli-id="'.$clinic_id.'">add patient</button>
+                                        <p class="doctor-panel-specs">'.$doctor_rows['specialization'].'</p>
+                                       </div>
+                                    </div>
+                                </div>';
+                                }
                
                             }
                         ?>
+
                         <div class="col-xs-12 col-md-6 col-md-offset-3">
                             <nav class="text-center">
                                 <ul class="pagination">
